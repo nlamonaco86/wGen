1404fd9e90ad0aa1a48c37173ce7f84fcf2306de
nlamonaco86 <nlamonaco86@gmail.com>
2020-08-20 12:31:54 -0400

    linked to handlebars, now customize the gen function

--- server.js
+++ server.js
@@ -2,19 +2,37 @@
 var express = require("express");
 // Allows us to run multiple "servers" if needed, boilerplate
 var app = express();
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
 
 var PORT = process.env.PORT || 3000;
 
 //set up for data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-//link to public directory for CSS/front end JS
-app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Nicky0416!",
+  database: "exerdb"
+});
+
+//render homepage with all exercises (for testing purposes)
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM exercises;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { exercises: data });
+  });
+});
 //require the html routes function
-require("./routes/htmlR")(app);
+// require("./routes/htmlR")(app);
 
 //run the server and log the port number to confirm it
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
